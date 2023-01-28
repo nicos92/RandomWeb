6b2590ed8cc21e8a8c6d345851fb0fa868137102
nicos92 <nicolassandoval140692@gmail.com>
2023-01-28 20:31:22 -0300

    agregado de convert xlsx

---
+++ js/convertXlsx.js
@@ -0,0 +1,42 @@
+/* set up XMLHttpRequest */
+
+$(document).ready(function(){
+
+    const url = "Datos.xlsx";
+    let oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+    
+    oReq.onload = function(e) 
+    {
+        let info = readData();
+        console.log(info);
+        
+        function readData()
+        {
+            let arraybuffer = oReq.response;
+
+            /* convert data to binary string */
+            let data = new Uint8Array(arraybuffer);
+            let arr = new Array();
+            for(let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+            let bstr = arr.join("");
+
+            /* Call XLSX */
+            let workbook = XLSX.read(bstr, {type:"binary"});
+
+            /* DO SOMETHING WITH workbook HERE */
+            let first_sheet_name = workbook.SheetNames[0];
+
+            /* Get worksheet */
+            let worksheet = workbook.Sheets[first_sheet_name];
+            let info = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+
+            return info;
+
+        }
+    }
+    
+    oReq.send();
+
+})
--- js/sorteoSectores.js
+++ js/sorteoSectores.js
@@ -3,87 +3,92 @@ document.addEventListener('DOMContentLoaded', () => {
     let contador = 0;
 
     sorteo.addEventListener("click", () => {
-        sectorSorteado.style.backgroundColor = "";
-
-        sectorSorteado.innerHTML = '.';
-
-        setTimeout( () => {
-            sectorSorteado.innerHTML = '..';
-        }, 500);
-
-        setTimeout( () => {
-            sectorSorteado.innerHTML = '...';
-        }, 1000);
-
-
-        setTimeout( () => {
-            const seleccion = setInterval(() => {
-                contador +=1;
-                let nroAzar = Math.floor(Math.random() * 11) + 1;
-                
-                switch (nroAzar) {
-                    case 1:
-                        sectorSorteado.innerHTML = '01 - Playa de faena';
-                        
-                        break;
-                    case 2:
-                        sectorSorteado.innerHTML = "02 - Cuarteo";
-                        
-                        break;
-                    case 3:
-                        sectorSorteado.innerHTML = "03 - Despostada";
-                        
-                        break;
-                    case 4:
-                        sectorSorteado.innerHTML = "04 - Menudencias";
-                        
-                        break;
-                    case 5:
-                        sectorSorteado.innerHTML = "05 - Triperia";
-                        
-                        break;
-                    case 6:
-                        sectorSorteado.innerHTML = "06 - Exteriores - Corrales";
-                        
-                        break;
-                    case 7:
-                        sectorSorteado.innerHTML = "07 - Playa de Emergencias";
-                        
-                        break;
-                    case 8:
-                        sectorSorteado.innerHTML = "08 - Melter";
-                        
-                        break;
-                    case 9:
-                        sectorSorteado.innerHTML = "09 - Lavadero de Roldanas";
-                        
-                        break;
-                    case 10:
-                        sectorSorteado.innerHTML = "10 - Nonatos";
-                        
-                        break;
-                    case 11:
-                        sectorSorteado.innerHTML = "11 - Saladero";
-                        
-                        break;
-                
-                    default:
-                        break;
-                }
-
-                if( contador > 20){
-                    clearInterval(seleccion);
-                    contador = 0;
-                    sectorSorteado.style.backgroundColor = "#AFEEEE";
-                }
-
-            }, 100)
-
-        }, 1500)
-
+        iniSorteo();
 
     })
-    
+
+
+function iniSorteo(){
+    sectorSorteado.style.backgroundColor = "";
+
+    sectorSorteado.innerHTML = '.';
+
+    setTimeout( () => {
+        sectorSorteado.innerHTML = '..';
+    }, 500);
+
+    setTimeout( () => {
+        sectorSorteado.innerHTML = '...';
+    }, 1000);
+
+
+    setTimeout( () => {
+        const seleccion = setInterval(() => {
+            contador +=1;
+            let nroAzar = Math.floor(Math.random() * 11) + 1;
+            
+            switch (nroAzar) {
+                case 1:
+                    sectorSorteado.innerHTML = '01 - Playa de faena';
+                    
+                    break;
+                case 2:
+                    sectorSorteado.innerHTML = "02 - Cuarteo";
+                    
+                    break;
+                case 3:
+                    sectorSorteado.innerHTML = "03 - Despostada";
+                    
+                    break;
+                case 4:
+                    sectorSorteado.innerHTML = "04 - Menudencias";
+                    
+                    break;
+                case 5:
+                    sectorSorteado.innerHTML = "05 - Triperia";
+                    
+                    break;
+                case 6:
+                    sectorSorteado.innerHTML = "06 - Exteriores - Corrales";
+                    
+                    break;
+                case 7:
+                    sectorSorteado.innerHTML = "07 - Playa de Emergencias";
+                    
+                    break;
+                case 8:
+                    sectorSorteado.innerHTML = "08 - Melter";
+                    
+                    break;
+                case 9:
+                    sectorSorteado.innerHTML = "09 - Lavadero de Roldanas";
+                    
+                    break;
+                case 10:
+                    sectorSorteado.innerHTML = "10 - Nonatos";
+                    
+                    break;
+                case 11:
+                    sectorSorteado.innerHTML = "11 - Saladero";
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+            if( contador > 20){
+                clearInterval(seleccion);
+                contador = 0;
+                sectorSorteado.style.fontWeight = "700";
+                sectorSorteado.style.backgroundColor = "#AFEEEE";
+            }
+
+        }, 100)
+
+    }, 1500)
+
+}
 
 
 });
